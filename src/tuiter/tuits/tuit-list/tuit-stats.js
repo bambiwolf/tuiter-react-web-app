6d87ff06b8263c5584bb5db2ab31bcd69ca03821
Bambi <bzhuangg@gmail.com>
2023-04-02 17:54:09 -0400

    Implementing dislike button!

--- src/tuiter/tuits/tuit-list/tuit-stats.js
+++ src/tuiter/tuits/tuit-list/tuit-stats.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {FaRegComment} from "react-icons/fa";
-import {AiOutlineHeart, AiOutlineRetweet} from "react-icons/ai";
+import {AiFillDislike, AiOutlineDislike, AiOutlineHeart, AiOutlineRetweet} from "react-icons/ai";
 import {BsShare} from "react-icons/bs";
 import {useDispatch} from "react-redux";
 import {likeTuit, unlikeTuit}
@@ -20,7 +20,9 @@ const TuitStats = (
             "replies": 0,
             "retuits": 0,
             "likes": 0,
-            "liked": false
+            "liked": false,
+            "dislikes": 0,
+            "disliked": false,
         }
     }
 ) => {
@@ -38,20 +40,30 @@ const TuitStats = (
     // if the tuit's boolean for liked is true, then the color of the heart should be red
     //<button className={"col-3 btn p-0 m-0"} onClick={handleLikeClick}> {heartIcon} {tuit.likes} </button>
     const heartIcon = tuit.liked ? <FcLike/> : <AiOutlineHeart/>;
+    const dislikeIcon = tuit.disliked ? <AiFillDislike/> : <AiOutlineDislike/>;
     const likeChange = tuit.liked ? tuit.likes - 1 : tuit.likes + 1;
+    const dislikeChange = tuit.disliked ? tuit.dislikes - 1 : tuit.dislikes + 1;
     return (
         <div className="row">
-            <div className={"col-3"}> <FaRegComment/> {tuit.replies} </div>
-            <div className={"col-3"}> <AiOutlineRetweet/> {tuit.retuits} </div>
+            <div className={"col-2"}> <FaRegComment/> {tuit.replies} </div>
+            <div className={"col-2"}> <AiOutlineRetweet/> {tuit.retuits} </div>
             <div className={"col-3"}>
-                Likes: {tuit.likes}
+                {tuit.likes}
                 <i onClick={() => dispatch(updateTuitThunk({
                     ...tuit,
                     likes: likeChange,
                     liked: !tuit.liked
-                }))} className="bi me-2 text-danger">{heartIcon}</i>
+                }))} className="bi me-2">{heartIcon}</i>
             </div>
-            <div className={"col-3"}> <BsShare/> </div>
+            <div className={"col-3"}>
+                {tuit.dislikes}
+                <i onClick={() => dispatch(updateTuitThunk({
+                    ...tuit,
+                    likes: dislikeChange,
+                    liked: !tuit.disliked
+                }))} className="bi me-2">{dislikeIcon}</i>
+            </div>
+            <div className={"col-2"}> <BsShare/> </div>
         </div>
     );
 };
